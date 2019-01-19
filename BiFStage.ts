e8fbfb0c931faf93683e9e87ae6591d1da578a38
Anwesh43 <anweshthecool0@gmail.com>
2019-01-20 03:21:15 +0530

    adding BiF class

--- BiFStage.ts
+++ BiFStage.ts
@@ -169,3 +169,26 @@ class BiFNode {
         return this
     }
 }
+
+class BiF {
+    root : BiFNode = new BiFNode(0)
+    curr : BiFNode = this.root
+    dir : number = 1
+
+    draw(context : CanvasRenderingContext2D) {
+        this.root.draw(context)
+    }
+
+    update(cb : Function) {
+        this.curr.update(() => {
+            this.curr = this.curr.getNext(this.dir, () => {
+                this.dir *= -1
+            })
+            cb()
+        })
+    }
+
+    startUpdating(cb : Function) {
+        this.curr.startUpdating(cb)
+    }
+}
